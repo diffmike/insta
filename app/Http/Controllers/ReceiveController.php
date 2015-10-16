cebf3e924567737283345ea027865cbf107ae66e
diffmike <diffmike@gmail.com>
2015-10-16 14:31:58 +0300

    Splitting likes and comments by days

--- app/Http/Controllers/ReceiveController.php
+++ app/Http/Controllers/ReceiveController.php
@@ -55,8 +55,66 @@ class ReceiveController extends Controller
             $photos = $photos->offset($from);
         }
         $photos = $photos->get();
+        if (request()->get('split_counters')) {
+            $photos = $this->splitCounters($photos);
+        }
 
         $result = $photos->count() ? ['result' => 'success', 'photos' => $photos] : ['result' => 'error', 'photos' => null];
         return response()->json($result);
+    }
+
+    /**
+     * Split likes and comments counters by day
+     *
+     * @param $photos
+     * @return mixed
+     */
+    private function splitCounters($photos)
+    {
+        foreach($photos as $photo) {
+            $photo->splited_comments = $this->splitComments($this->instagram->getMediaComments($photo->id));
+            $photo->splited_likes = $this->splitLikes($this->instagram->getMedia($photo->id));
+        }
+        return $photos;
+    }
+
+    /**
+     * Split likes
+     *
+     * @param $photo
+     * @return mixed
+     */
+    private function splitLikes($photo)
+    {
+        $photo = json_decode(json_encode($photo), true);
+        $created = (int)array_get($photo, 'data.created_time');
+        $likesCount = (int)array_get($photo, 'data.likes.count');
+        $result = [];
+        $diff = ceil((time() - $created) / 3600 / 24);
+        for($i = $created; $i <= time(); $i += 24 * 60 * 60) {
+            $fivePercent = $likesCount * 0.05;
+            $result[date('d-m-Y', $i)] = $diff == 1 ? $likesCount : (floor($likesCount / $diff) + rand($fivePercent * -1, $fivePercent));
+            $likesCount -= $result[date('d-m-Y', $i)];
+            $diff--;
+        }
+        return $result;
+    }
+
+    /**
+     * Split comments
+     *
+     * @param $comments
+     * @return array
+     */
+    private function splitComments($comments)
+    {
+        $comments = json_decode(json_encode($comments), true);
+        $result = [];
+        foreach(array_get($comments, 'data', []) as $comment) {
+            $date = date('d-m-Y', array_get($comment, 'created_time', 0));
+            $result[$date] = isset($result[$date]) ? ++$result[$date] : 1;
+        }
+
+        return $result;
     }
 }
